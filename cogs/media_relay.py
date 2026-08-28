d940e57b79167becb674c1969b08797b0e32edef
Spad1964 <g73p04.productions@gmail.com>
2026-08-28 12:11:45 +0100

    fix: added logs for debugging purposes

--- cogs/media_relay.py
+++ cogs/media_relay.py
@@ -32,10 +32,28 @@ class MediaRelay(commands.Cog):
             log.warning("Media relay target channel %s not found", cfg.target_channel_id)
             return
 
+        if len(attachments) > _MAX_FILES_PER_MESSAGE:
+            log.info(
+                "Message %s in channel %s has %d attachments; relaying first %d",
+                message.id,
+                message.channel.id,
+                len(attachments),
+                _MAX_FILES_PER_MESSAGE,
+            )
         attachments = attachments[:_MAX_FILES_PER_MESSAGE]
+
+        log.info(
+            "Relaying %d attachment(s) from %s (msg %s, channel %s) to channel %s",
+            len(attachments),
+            message.author,
+            message.id,
+            message.channel.id,
+            target.id,
+        )
         try:
             files = [await a.to_file() for a in attachments]
             await target.send(files=files)
+            log.info("Relayed msg %s to channel %s", message.id, target.id)
         except discord.Forbidden:
             log.error("Missing permissions to relay media to channel %s", target.id)
         except discord.HTTPException as exc:
